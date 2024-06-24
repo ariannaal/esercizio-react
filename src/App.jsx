c389088e58a2209504b45c29ba3bb98f2f65a646
ariannaal <ariannaloreti25@gmail.com>
2024-06-24 17:11:28 +0200

    adds pictures

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import ButtonComponent from './components/ButtonComponent';
 import MainComponent from './components/ImageComponent';
 import ArticleComponent from './components/ArticleComponent';
+import PicturesComponents from './components/PicturesComponents';
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
         <ButtonComponent text="Click here" reason="to find out more about Persian cats" />
         <ArticleComponent info="Persian cats are a long-haired breed known for their elegant appearance and gentle temperament. They have a distinctive round face with a short muzzle, large expressive eyes, and a plush, luxurious coat that comes in various colors and patterns. Originating from Persia (modern-day Iran), these cats have been cherished as pets for centuries. Persians are typically calm and affectionate, preferring a serene environment and bonding closely with their owners. Due to their long fur, they require regular grooming to prevent matting and maintain their coat's beauty. Their docile and sweet nature makes them excellent companions.
 " breed="About the breed"/>
-        
+        <PicturesComponents/>
       </header>
       
       
---
+++ src/components/PicturesComponents.jsx
@@ -0,0 +1,15 @@
+function PicturesComponents() {
+    return (
+        <div className="picturesContainer">
+        <img src="https://media.istockphoto.com/id/467325740/it/foto/tenuta-persiano-gattino-carino-colourpoint-%C3%A8-seduta-frontale.webp?b=1&s=170667a&w=0&k=20&c=Wm23ZDTArGG2KJp8SFk0MG-2TGAES9YKfJQZKOQ4z2M=" alt="first-picture" className="pictureCats" />
+          <img src="https://media.istockphoto.com/id/108273755/it/foto/gatto-persiano-con-occhi-arancioni-realizzazione-isolato-su-bianco.webp?b=1&s=170667a&w=0&k=20&c=dkuJic8YShfyUj23BGUO19vCf0YWwAfBFUnB_v7u7vw=" alt="second-picture" className="pictureCats" />
+            <img src="https://catexplore.com/wp-content/uploads/2022/05/Persian-cat-red-dionoanomalia-Adobe80632306-858x1024.jpg" alt="third-picture" className="pictureCats" />
+            <img src="" alt="" />  
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYgj3L7fxZkWbJdVUTzJLhsHBS9UZ5Lw4Wqg&s" alt="fourth-picture" className="pictureCats" />
+            <img src="" alt="" /> 
+            
+        </div>
+    )
+}
+
+export default PicturesComponents;
